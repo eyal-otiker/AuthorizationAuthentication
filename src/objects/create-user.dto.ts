7b896218cf0d46e1162729c8ab8ff448526f9537
eyal <eyal13102@gmail.com>
2025-12-02 13:31:03 +0200

    [ADD] User Schema

---
+++ src/objects/create-user.dto.ts
@@ -0,0 +1,12 @@
+import { IsEmail, IsString } from "class-validator";
+
+export class CreateUserDto {
+    @IsString()
+    userName: string;
+    
+    @IsString()
+    password: string;
+
+    @IsEmail()
+    email: string;
+}
